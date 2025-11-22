dc66dfb05f6e80fceb2def63c7c961068ae94618
TheusOliverse <matheusoliveira8114@gmail.com>
2025-11-22 15:54:08 -0400

    animações adicionadas

--- script.js
+++ script.js
@@ -16,10 +16,75 @@ document.getElementById("achar_perio").addEventListener('click', () => {
     if (select.value === 'o1') {
         document.getElementById("periodo1").style.display = '';
         document.getElementById("periodo2").style.display = 'none';
-    } else if(select.value === 'o2') {
+    } else if (select.value === 'o2') {
         document.getElementById("periodo2").style.display = '';
         document.getElementById("periodo1").style.display = 'none';
     } else {
         alert("Erro!")
+    }
+})
+
+
+//animar
+let background_header = document.getElementById("background_header");
+let titulo_header = document.getElementById("titulo_header");
+let subtitulo = document.getElementById("subtit_header");
+let abaixo_do_subtitulo = document.getElementById("p_header_tit");
+
+let but_ini = document.getElementById("but_ini");
+let conv = document.getElementById("conv");
+
+const elementos_animados = {
+    'background_header': background_header,
+    'titulo': titulo_header,
+    'subtitulo': subtitulo,
+    'abaixo_do_subtitulo': abaixo_do_subtitulo,
+    'botoes_iniciais': but_ini,
+    'conv': conv
+}
+
+const classes = {
+    'background_header': 'background_header',
+    'titulo': 'titulo_header',
+    'subtitulo': 'subtitulo_header',
+    'abaixo_do_subtitulo': 'abaixo_do_subtitulo',
+
+    'botoes_iniciais': 'but_ini',
+    'conv': 'conv'
+}
+
+window.addEventListener('scroll', () => {
+
+    elementos_animados.conv.style.transition = '1s';
+
+    if (window.scrollY < 100) {
+        elementos_animados.background_header.classList.remove(`${classes.background_header}`);
+        elementos_animados.titulo.classList.remove(`${classes.titulo}`);
+        elementos_animados.subtitulo.classList.remove(`${classes.subtitulo}`);
+        elementos_animados.abaixo_do_subtitulo.classList.remove(`${classes.abaixo_do_subtitulo}`);
+
+        elementos_animados.botoes_iniciais.style.gap = '20px';
+        elementos_animados.botoes_iniciais.style.opacity = '1';
+    } else {
+        elementos_animados.background_header.classList.add(`${classes.background_header}`);
+        elementos_animados.titulo.classList.add(`${classes.titulo}`);
+        elementos_animados.subtitulo.classList.add(`${classes.subtitulo}`);
+        elementos_animados.abaixo_do_subtitulo.classList.add(`${classes.abaixo_do_subtitulo}`);
+
+        elementos_animados.botoes_iniciais.style.gap = '100px';
+        elementos_animados.botoes_iniciais.style.opacity = '0';
+    }
+})
+
+elementos_animados.conv.style.opacity = '0';
+window.addEventListener('scroll', () => {
+
+    elementos_animados.botoes_iniciais.style.transition = '1s';
+
+
+    if (window.scrollY < 200) {
+        elementos_animados.conv.style.opacity = '0';
+    } else {
+        elementos_animados.conv.style.opacity = '1';
     }
 })
