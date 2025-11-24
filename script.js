6b245aa5289b25ea246208b151b62c403f26e5f2
TheusOliverse <matheusoliveira8114@gmail.com>
2025-11-24 08:22:32 -0400

    adição de mais elementos no site

--- script.js
+++ script.js
@@ -24,8 +24,7 @@ document.getElementById("achar_perio").addEventListener('click', () => {
     }
 })
 
-
-//animar
+//animar (Início do site)
 let background_header = document.getElementById("background_header");
 let titulo_header = document.getElementById("titulo_header");
 let subtitulo = document.getElementById("subtit_header");
@@ -34,13 +33,19 @@ let abaixo_do_subtitulo = document.getElementById("p_header_tit");
 let but_ini = document.getElementById("but_ini");
 let conv = document.getElementById("conv");
 
+// caixas de informações (animações)
+let box_info_1 = document.getElementById("box_inf1");
+let box_info_2 = document.getElementById("box_inf2");
+let box_info_3 = document.getElementById("box_inf3");
+
 const elementos_animados = {
     'background_header': background_header,
     'titulo': titulo_header,
     'subtitulo': subtitulo,
     'abaixo_do_subtitulo': abaixo_do_subtitulo,
+
     'botoes_iniciais': but_ini,
-    'conv': conv
+    'conv': conv,
 }
 
 const classes = {
@@ -50,21 +55,46 @@ const classes = {
     'abaixo_do_subtitulo': 'abaixo_do_subtitulo',
 
     'botoes_iniciais': 'but_ini',
-    'conv': 'conv'
+    'conv': 'conv',
+
 }
 
+//Código contendo as tags das caixas informativas
+const box = {
+    'box1': box_info_1,
+    'box2': box_info_2,
+    'box3': box_info_3
+}
+
+box.box1.style.transition = '1.5s';
+box.box2.style.transition = '2s';
+box.box3.style.transition = '2.5s';
+
+box_info_1.style.opacity = '0';
+box_info_2.style.opacity = '0';
+box_info_3.style.opacity = '0';
+
+//Animações com o Scrool
 window.addEventListener('scroll', () => {
 
-    elementos_animados.conv.style.transition = '1s';
+    //Caixa de direcionamento inicial
+    elementos_animados.conv.style.transition = '4s';
 
     if (window.scrollY < 100) {
+        //Parte I (Início)
         elementos_animados.background_header.classList.remove(`${classes.background_header}`);
         elementos_animados.titulo.classList.remove(`${classes.titulo}`);
         elementos_animados.subtitulo.classList.remove(`${classes.subtitulo}`);
         elementos_animados.abaixo_do_subtitulo.classList.remove(`${classes.abaixo_do_subtitulo}`);
-
+        
+        //Parte II (Botões interativos)
         elementos_animados.botoes_iniciais.style.gap = '20px';
         elementos_animados.botoes_iniciais.style.opacity = '1';
+
+        //Box de informações
+        box_info_1.style.opacity = '0';
+        box_info_2.style.opacity = '0';
+        box_info_3.style.opacity = '0';
     } else {
         elementos_animados.background_header.classList.add(`${classes.background_header}`);
         elementos_animados.titulo.classList.add(`${classes.titulo}`);
@@ -73,6 +103,10 @@ window.addEventListener('scroll', () => {
 
         elementos_animados.botoes_iniciais.style.gap = '100px';
         elementos_animados.botoes_iniciais.style.opacity = '0';
+
+        box_info_1.style.opacity = '1';
+        box_info_2.style.opacity = '1';
+        box_info_3.style.opacity = '1';
     }
 })
 
@@ -81,10 +115,33 @@ window.addEventListener('scroll', () => {
 
     elementos_animados.botoes_iniciais.style.transition = '1s';
 
-
     if (window.scrollY < 200) {
         elementos_animados.conv.style.opacity = '0';
     } else {
         elementos_animados.conv.style.opacity = '1';
+    }
+})
+
+//Main (conteúdo principal)
+//Cursos (interações e atividade)
+let cursos = document.getElementById("cursos");
+cursos.style.transition = '2s';
+cursos.style.opacity = '0';
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY < 600) {
+        cursos.style.opacity = '0';
+    } else {
+        cursos.style.opacity = '1';
+    }   
+})
+
+//Icon go
+window.addEventListener('scroll', () => {
+    let icon_go = document.getElementById("ani_go");
+    if (window.scrollY < 100) {
+        icon_go.classList.remove("ani_go2");
+    } else {
+        icon_go.classList.add("ani_go2");
     }
 })
